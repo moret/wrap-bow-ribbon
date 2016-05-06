4a8f846150bdbe050649e0a1a8662d567d15e2d2
Danilo Moret <github@moret.pro.br>
2016-05-06 12:02:50 -0300

    extract styles to external stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: './dist/bundle.js'
   },
+  plugins: [
+    new ExtractTextPlugin('./dist/styles.css', {
+      allChunks: true
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel' },
-      { test: /\.css$/, loader: 'style!css' }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css') }
     ]
   }
 };
