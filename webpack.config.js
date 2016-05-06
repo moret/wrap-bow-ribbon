b288dc290220f3e0f73f571ed3c8fa638fa90b4c
Danilo Moret <github@moret.pro.br>
2016-05-06 11:48:02 -0300

    move webpack setup to configuration file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel' }
+    ]
+  }
+};
